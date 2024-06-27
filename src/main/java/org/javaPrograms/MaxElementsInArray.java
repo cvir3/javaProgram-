4dfe7c9aaae5906c7082432861d66c5a0b1e3af4
Viren Chauhan <vir3.chauhan@gmail.com>
2024-06-27 23:57:18 +0530

    Find Maximum values in Array

---
+++ src/main/java/org/javaPrograms/MaxElementsInArray.java
@@ -0,0 +1,16 @@
+package org.javaPrograms;
+
+public class MaxElementsInArray {
+    public static void main(String[] args) {
+        int a[] = {50, 100, 40, 20, 60};
+        int max = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        System.out.println("Maximum Element is array is:" + max);
+    }
+
+}
